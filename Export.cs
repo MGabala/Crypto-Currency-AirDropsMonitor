25728ceb026e52b9ec11c0b77c4b2e88a001c0ab
gugimistrz666 <82496006+MGabala@users.noreply.github.com>
2022-04-24 14:14:13 +0200

    Add project files.

---
+++ Export.cs
@@ -0,0 +1,63 @@
+﻿using HtmlAgilityPack;
+
+namespace MyApp
+{
+    internal class Export
+    {
+
+        public static void ExportFromSites()
+        {
+            TrustedLines();
+            TrustLinesKnownAirdrops();
+            PossibleAirdrops();
+        }
+
+        private static void PossibleAirdrops()
+        {
+            //pobieranie z xumm.community/tokens
+            var file = @"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\xummcommunitytokens.htm";
+            var html_ = file;
+            var web = new HtmlWeb();
+            var xd = web.Load(html_);
+            var names = xd.DocumentNode.SelectNodes("//*[contains(@class,'mat-cell cdk-cell cdk-column-currencyCodeUTF8 mat-column-currencyCodeUTF8 ng-tns-c182-1 ng-star-inserted')]").Select(x => x.FirstChild);
+            foreach (var name in names)
+            {
+                File.AppendAllText(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\xummtokens.txt", name.InnerText.Trim() + "\n");
+            }
+
+        }
+
+        private static void TrustedLines()
+        {
+            //pobieranie z xrpscan.com
+            var file = @"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\matikwallet.htm";
+
+            var html_ = file;
+            var web = new HtmlWeb();
+            var xd = web.Load(html_);
+            var names = xd.DocumentNode.SelectNodes("//*[contains(@class,'text-monospace')]").Select(x => x.FirstChild);
+            //zapisywanie do pliku nazw połączonych trustlinów
+            foreach (var name in names.Skip(1))
+            {
+
+                File.AppendAllText(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\xrpscan.txt", name.InnerText + "\n");
+            }
+
+        }
+
+        private static void TrustLinesKnownAirdrops()
+        {
+            //pobieranie z xrpltools.com/#/airdrops
+            var file = @"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\airdrop1.htm";
+            var html_ = file;
+            var web = new HtmlWeb();
+            var xd = web.Load(html_);
+            var names = xd.DocumentNode.SelectNodes("//*[contains(@class,'el-table_1_column_2 is-left')]").Select(x => x.FirstChild);
+            foreach (var name in names)
+            {
+                File.AppendAllText(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\xrpltoolslist.txt", name.InnerText.Trim() + "\n");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program : Export
+    {
+        static void Main(string[] args)
+        {
+             ExportFromSites();
+              ShowData();
+          
+        }
+
+     
+
+        private static void ShowData()
+        {
+            
+            //wallet
+            var wallet_z_trustami = File.ReadAllLines(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\xrpscan.txt");
+            //lista trustów
+           var trusty_do_polaczenia =  File.ReadAllLines(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\xummtokens.txt");
+            var list_of_new_coins = new List<string>();
+            var list_of_old_coins = new List<string>();
+            foreach(var newcoin in trusty_do_polaczenia)
+            {
+                list_of_new_coins.Add(newcoin);
+            }
+            foreach(var oldcoin in wallet_z_trustami)
+            {
+                list_of_old_coins.Add(oldcoin);
+            }
+            var new_list = new List<string>();
+            new_list = list_of_new_coins.Except(list_of_old_coins).ToList();
+            foreach(var item in new_list)
+            {
+                File.AppendAllText(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\newcoins.txt", item+"\n");
+            }
+            
+            //for (int i = 0; i < trusty_do_polaczenia.Count(); i++)
+            //{
+            //    for (int y = 0; y < wallet_z_trustami.Count(); y++)
+            //    {
+            //        Console.WriteLine(trusty_do_polaczenia[i]+":"+wallet_z_trustami[y]);
+            //        if (wallet_z_trustami[y] != trusty_do_polaczenia[i])
+            //        {
+            //            File.AppendAllText(@"C:\Users\sam_sepi0l\Desktop\CheckAirdrops\CheckAirdrops\newcoins.txt", trusty_do_polaczenia[i] + "\n");
+            //        }
+            //        else Console.WriteLine($"else + {trusty_do_polaczenia[i]}");
+                   
+            //    }
+            //}
+  
+        }
+    }
+}
